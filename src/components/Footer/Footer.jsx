01d5df80f130ae1018a0f099f971248b72390832
Kyria <kyria.bacinski@gmail.com>
2025-12-05 08:48:54 -0700

    Update footer contact information and copyright

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -31,8 +31,7 @@ function Footer() {
         <section className="contact__info">
           <div className="contact__group">   
             <h4 className="contact__company">Cyclea Management</h4>
-            <h4 className="contact__address">0620 Capilano,</h4>
-            <h4>Edmonton, AB</h4>
+            <h4>YEG AB</h4>
             <h4>CANADA</h4>
             <a href="mailto:info@cyclea.com">
               <h4 className="contact__email">info@cyclea.com</h4>
@@ -42,7 +41,7 @@ function Footer() {
         <Link to="/">
         <img className="contact__logo contact__logo--2" src={logo} alt="Cyclea logo"/> 
         </Link>
-        <h4 className="contact__copyright">Copyright Kyria Bacinski © 2025 All Rights Reserved</h4>
+        <h4 className="contact__copyright">Copyright KB © 2025 All Rights Reserved</h4>
       </footer>
     );
 }
